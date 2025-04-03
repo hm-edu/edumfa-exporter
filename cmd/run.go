f5002578833be0e94327c7be6a30b8af2a10aba4
Florian Ritterhoff <florian.ritterhoff@hm.edu>
2025-04-03 15:52:02 +0200

    feat: handle errors better

--- cmd/run.go
+++ cmd/run.go
@@ -17,17 +17,62 @@ import (
 )
 
 var db string
+var usage *prometheus.GaugeVec
+var users prometheus.Gauge
+
+func updateData() {
+	db, err := sql.Open("mysql", db)
+	if err != nil {
+		slog.Error("failed creating connection query", slog.Any("error", err))
+		return
+	}
+	//nolint:errcheck
+	defer db.Close()
+	rows, err := db.Query("select tokentype, tokeninfo.key, count(*) from token left join tokeninfo on token.id = tokeninfo.token_id and tokeninfo.key='passkey' group by tokentype, tokeninfo.key")
+	if err != nil {
+		slog.Error("failed executing query", slog.Any("error", err))
+		return
+	}
+	//nolint:errcheck
+	defer rows.Close()
+	for rows.Next() {
+		var tokentype, key string
+		var count int
+		if err := rows.Scan(&tokentype, &key, &count); err != nil {
+			slog.Error("failed parsing line", slog.Any("error", err))
+			return
+		}
+		if key == "passkey" {
+			tokentype = key
+		}
+		usage.WithLabelValues(tokentype).Set(float64(count))
+	}
+	rows, err = db.Query("select count(distinct user_id) from tokenowner")
+	if err != nil {
+		slog.Error("failed executing query", slog.Any("error", err))
+		return
+	}
+	//nolint:errcheck
+	defer rows.Close()
+	for rows.Next() {
+		var count int
+		if err := rows.Scan(&count); err != nil {
+			slog.Error("failed parsing line", slog.Any("error", err))
+			return
+		}
+		users.Set(float64(count))
+	}
+}
 
 // runCmd represents the run command
 var runCmd = &cobra.Command{
 	Use: "run",
 	Run: func(cmd *cobra.Command, args []string) {
-		usage := prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "token"}, []string{"model"})
-		users := prometheus.NewGauge(prometheus.GaugeOpts{Name: "user"})
+		usage = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "token"}, []string{"model"})
+		users = prometheus.NewGauge(prometheus.GaugeOpts{Name: "user"})
 		prometheus.MustRegister(usage)
 		prometheus.MustRegister(users)
 		s, err := gocron.NewScheduler()
-
 		if err != nil {
 			panic(err)
 		}
@@ -38,47 +83,13 @@ var runCmd = &cobra.Command{
 		}(s)
 
 		job, err := s.NewJob(gocron.CronJob("*/2 * * * *", false), gocron.NewTask(func() {
-			db, err := sql.Open("mysql", db)
-			if err != nil {
-				panic(err)
-			}
-			//nolint:errcheck
-			defer db.Close()
-			rows, err := db.Query("select tokentype, tokeninfo.key, count(*) from token left join tokeninfo on token.id = tokeninfo.token_id and tokeninfo.key='passkey' group by tokentype, tokeninfo.key")
-			if err != nil {
-				panic(err)
-			}
-			//nolint:errcheck
-			defer rows.Close()
-			for rows.Next() {
-				var tokentype, key string
-				var count int
-				if err := rows.Scan(&tokentype, &key, &count); err != nil {
-					panic(err)
-				}
-				if key == "passkey" {
-					tokentype = key
-				}
-				usage.WithLabelValues(tokentype).Set(float64(count))
-			}
-			rows, err = db.Query("select count(distinct user_id) from tokenowner")
-			if err != nil {
-				panic(err)
-			}
-			//nolint:errcheck
-			defer rows.Close()
-			for rows.Next() {
-				var count int
-				if err := rows.Scan(&count); err != nil {
-					panic(err)
-				}
-				users.Set(float64(count))
-			}
+			updateData()
 		}))
 		if err != nil {
 			panic(err)
 		}
 		slog.Info("job created", slog.String("job", job.ID().String()))
+		updateData()
 		http.Handle("/metrics", promhttp.Handler())
 		//nolint:errcheck
 		http.ListenAndServe(":8080", nil)
